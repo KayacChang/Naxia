fe36c31bd90cd5cfe10792243c5e2744575961ac
kayacchang <egg734631@gmail.com>
2021-02-28 16:33:57 +0800

    remove unuse

--- packages/client/src/components/Modal.tsx
+++ packages/client/src/components/Modal.tsx
@@ -2,13 +2,8 @@ import { ReactNode } from "react";
 
 type ModalProps = {
   children: ReactNode;
-  open?: boolean;
 };
-export function Modal({ children, open = true }: ModalProps) {
-  if (!open) {
-    return <></>;
-  }
-
+export function Modal({ children }: ModalProps) {
   return (
     <div className="absolute top-0 right-0 w-full h-full bg-black bg-opacity-50 pointer-events-auto">
       {children}
