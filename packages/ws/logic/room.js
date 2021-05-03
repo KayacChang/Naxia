e3e3082b733f3050cf0c7649d5c56c33c4f46c44
kayacchang <egg734631@gmail.com>
2021-05-03 14:55:22 +0800

    git subrepo clone --force https://github.com/kayac-chang/naxia-game-service.git packages/ws
    subrepo:
      subdir:   "packages/ws"
      merged:   "b427756"
    upstream:
      origin:   "https://github.com/kayac-chang/naxia-game-service.git"
      branch:   "master"
      commit:   "b427756"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- packages/ws/logic/room.js
+++ packages/ws/logic/room.js
@@ -1,11 +1,11 @@
-import R from "ramda";
-import { interval } from "rxjs";
-import { map, share } from "rxjs/operators";
+import { BehaviorSubject, interval } from "rxjs";
+import { filter, map, share, throttleTime, mergeWith } from "rxjs/operators";
 import { Random, MersenneTwister19937 } from "random-js";
 
 const random = new Random(MersenneTwister19937.autoSeed());
 
-const TimeInterval = 1000;
+const Second = 1000;
+const TimeInterval = 1 * Second;
 
 const RoomStatus = {
   Change: 0, // 洗牌中
@@ -100,50 +100,70 @@ const results = [
   },
 ];
 
-const Count = (time) =>
-  R.pipe(R.reverse, R.map(Event.CountDown))(R.range(0, time));
-
-const GameLoop = {
-  [RoomStatus.Change]: [Event.RoomStatus(RoomStatus.Change), ...Count(10)],
-  [RoomStatus.Start]: [Event.RoomStatus(RoomStatus.Start), ...Count(10)],
-  [RoomStatus.Stop]: [Event.RoomStatus(RoomStatus.Stop), ...Count(10)],
-  [RoomStatus.Result]: [
-    [
+function* GameStatus() {
+  while (true) {
+    yield* [
+      Event.RoomStatus(RoomStatus.Change),
+      Event.RoomStatus(RoomStatus.Start),
+      Event.RoomStatus(RoomStatus.Stop),
       Event.RoomStatus(RoomStatus.Result),
-      Event.RoundResult(results[1]),
-      Event.Boss({
-        id: 9,
-        name: "骷顱人",
-        spine: "https://storage.googleapis.com/naxia-dev/guaiwu1.png",
-        spine_json: "https://storage.googleapis.com/naxia-dev/guaiwu1.json",
-        spine_atlas: "https://storage.googleapis.com/naxia-dev/guaiwu1.atlas",
-        hp: 1,
-        rate: "30.0000",
-      }),
-    ],
-    ...Count(10),
-  ],
-};
+    ];
+  }
+}
 
-const GameEvents = Object.values(GameLoop).flat();
+function GameLogic() {
+  return [
+    Event.RoundResult(random.pick(results)),
+    Event.Boss({
+      id: 9,
+      name: "骷顱人",
+      spine: "https://storage.googleapis.com/naxia-dev/guaiwu1.png",
+      spine_json: "https://storage.googleapis.com/naxia-dev/guaiwu1.json",
+      spine_atlas: "https://storage.googleapis.com/naxia-dev/guaiwu1.atlas",
+      hp: 1,
+      rate: "30.0000",
+    }),
+  ];
+}
 
-const Room = interval(TimeInterval).pipe(
-  map((value) => GameEvents[value % GameEvents.length]),
-  share()
+const Loop = interval(TimeInterval).pipe(share());
+
+const Count = Loop.pipe(
+  map((time) => Event.CountDown(10 - (time % 10))),
+  share({ connector: () => new BehaviorSubject() })
 );
 
-Room.subscribe(console.log);
+const status = GameStatus();
+const Status = Loop.pipe(
+  throttleTime(10 * Second),
+  map(() => status.next().value),
+  share({ connector: () => new BehaviorSubject() })
+);
+
+const Logic = Status.pipe(
+  filter(Boolean),
+  filter(({ data }) => data.status === RoomStatus.Result),
+  map(GameLogic),
+  share({ connector: () => new BehaviorSubject() })
+);
+
+Status.subscribe(console.log);
+Count.subscribe(console.log);
+Logic.subscribe(console.log);
 
 export function join(connection) {
-  Room.subscribe((event) => {
+  function send(event) {
     if (Array.isArray(event)) {
       return event.forEach((event) =>
         connection.socket.send(JSON.stringify(event))
       );
     }
-
     return connection.socket.send(JSON.stringify(event));
-  });
+  }
+
+  Count.subscribe(send);
+  Status.subscribe(send);
+  Logic.subscribe(send);
 }
 
 export default { join };
