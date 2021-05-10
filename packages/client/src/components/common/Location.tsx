27f0f7981b2261615cabd4cad43eec1c78ee1724
sherin.lin <sherin.lin@car-plus.com.tw>
2021-05-10 22:43:10 +0800

    drop

--- packages/client/src/components/common/Location.tsx
+++ packages/client/src/components/common/Location.tsx
@@ -1,18 +1,18 @@
 import Assets from "assets";
 
 type LocationProps = {
-    value: string;
+  value: string;
 };
 export function Location({ value }: LocationProps) {
-    return (
-        <div className="absolute left-1/2 transform -translate-x-1/2 z-50">
-            <div className="relative w-52 text-white">
-                <img src={Assets.Common.Location} alt="world frame" />
+  return (
+    <div className="absolute left-1/2 transform -translate-x-1/2 z-50">
+      <div className="relative w-52 text-white">
+        <img src={Assets.Common.Location} alt="world frame" />
 
-                <h1 className="absolute top-0 h-full w-full flex items-center justify-center font-kai text-fansy text-xl tracking-wider">
-                    {value}
-                </h1>
-            </div>
-        </div>
-    );
+        <h1 className="absolute top-0 h-full w-full flex items-center justify-center font-kai text-fansy text-xl tracking-wider">
+          {value}
+        </h1>
+      </div>
+    </div>
+  );
 }
