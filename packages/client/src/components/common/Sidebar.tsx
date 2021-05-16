6d723d27189ea93ac068685fceaf24f1714a201e
sherin.lin <sherin.lin@car-plus.com.tw>
2021-05-16 11:26:10 +0800

    road done

--- packages/client/src/components/common/Sidebar.tsx
+++ packages/client/src/components/common/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Announcement, RoomRoad, Setting } from "components";
+import { Announcement, Setting } from "components";
 
 type SidebarProps = {
   className?: string;
@@ -9,8 +9,6 @@ export function Sidebar({ className }: SidebarProps) {
       <Announcement />
 
       <Setting />
-
-      {/* <RoomRoad className="w-full h-full" rounds={[]} /> */}
     </aside>
   );
 }
