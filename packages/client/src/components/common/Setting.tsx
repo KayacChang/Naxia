2f0c178ff8ae95f29e2845942e9136ea4f880969
kayacchang <egg734631@gmail.com>
2021-05-18 00:12:43 +0800

    refactor setting

--- packages/client/src/components/common/Setting.tsx
+++ packages/client/src/components/common/Setting.tsx
@@ -1,51 +1,68 @@
 import { useState } from "react";
 import Assets from "assets";
 import { Modal, SystemModal } from "components";
+import { Range } from "react-range";
 
-type SliderBarProps = {
-  title: string;
+type InputRangeProps = {
+  label: string;
   value: number;
-  setValue: any;
+  min?: number;
+  max?: number;
+  step?: number;
+  onChange: (value: number) => void;
 };
-function SliderBar({ title, value, setValue }: SliderBarProps) {
-  function soundValueHandler(event: React.FormEvent<HTMLInputElement>) {
-    event.preventDefault();
-    setValue(parseInt(event.currentTarget.value));
-  }
-
+function InputRange({
+  label,
+  value,
+  onChange,
+  min = 0,
+  max = 100,
+  step = 1,
+}: InputRangeProps) {
   return (
-    <div className="flex-1 text-base text-yellow-400 flex justify-between">
-      <p>{title}</p>
-      <div className="flex justify-center items-center">
-        <div className="w-40 relative flex justify-center items-center">
-          <img
-            className="w-40"
-            src={Assets.Common.Setting_Volume_Bottom}
-            alt="setting volume bottom"
-          />
-          <div
-            className="absolute h-full left-0 overflow-hidden bg-no-repeat bg-auto"
-            style={{
-              top: "15%",
-              left: "0.24rem",
-              width: value < 60 ? value + "%" : value - 5 + "%",
-              height: "90%",
-              backgroundImage: "url(" + Assets.Common.Setting_Volume_Bar + ")",
-              backgroundSize: "auto 100%",
-            }}
-          ></div>
-          <div className="absolute">
-            <input
-              className="w-40"
-              type="range"
-              min="0"
-              max="100"
-              step="1"
-              value={value}
-              onChange={(e) => soundValueHandler(e)}
+    <div className="flex-1 flex items-center space-x-8">
+      <p className="whitespace-nowrap">{label}</p>
+
+      <div className="relative flex items-center">
+        <img src={Assets.Common.Setting_Volume_Bottom} alt="background" />
+
+        <Range
+          step={step}
+          min={min}
+          max={max}
+          values={[value]}
+          onChange={([value]) => onChange(value)}
+          renderTrack={({ props, children }) => (
+            <div className="absolute">
+              <div
+                {...props}
+                className="relative mx-1.5 pointer-events-auto"
+                style={{
+                  ...props.style,
+                }}
+              >
+                <img
+                  style={{
+                    transform: `translate(0, 1px)`,
+                    clipPath: `inset(0 ${100 - value}% 0 0)`,
+                  }}
+                  src={Assets.Common.Setting_Volume_Bar}
+                  alt="bar"
+                />
+
+                {children}
+              </div>
+            </div>
+          )}
+          renderThumb={({ props }) => (
+            <img
+              {...props}
+              className="absolute top-0 w-5"
+              src={Assets.Common.Setting_Volume_Controller}
+              alt="controller"
             />
-          </div>
-        </div>
+          )}
+        />
       </div>
     </div>
   );
@@ -65,7 +82,6 @@ export function Setting() {
   function resetVolume() {
     setSoundValue(80);
     setMusicValue(80);
-    setSettingOpen(false);
   }
 
   return (
@@ -74,64 +90,38 @@ export function Setting() {
         <div className="absolute text-yellow-100 text-shadow tracking-wider bottom-2 right-0 text-sm">
           設定
         </div>
+
         <button onClick={() => setSettingOpen(true)}>
           <img src={icon.icons.normal} alt={icon.key} />
         </button>
       </div>
-      <div className="absolute left-0">
-        {isSettingOpen && (
-          <Modal onClose={() => setSettingOpen(false)}>
-            <SystemModal
-              title="設定"
-              button="確認"
-              subButton="還原預設"
-              onConfirm={() => setSettingOpen(false)}
-              customFunc={resetVolume}
-              onClose={() => setSettingOpen(false)}
-            >
-              <div className="h-full flex font-noto text-xs text-white">
-                <div className="flex-1 flex flex-wrap justify-center items-center px-14 py-10">
-                  <SliderBar
-                    title="音效音量"
-                    value={soundValue}
-                    setValue={setSoundValue}
-                  />
 
-                  <SliderBar
-                    title="音樂音量"
-                    value={musicValue}
-                    setValue={setMusicValue}
-                  />
-                </div>
-              </div>
-            </SystemModal>
-          </Modal>
-        )}
-        <style>
-          {`
-          input[type="range"] {
-            -webkit-appearance: none;
-            background: none;
-            outline: none;
-            border-radius: 0;
-            overflow: hidden;
-            cursor: pointer;
-          }
-          input[type="range"]::-webkit-slider-thumb {
-            -webkit-appearance: none;
-            margin-top:0.2rem;
-            width: 1rem;
-            height: 1rem;
-            background-image:
-                              url(` +
-            Assets.Common.Setting_Volume_Controller +
-            `);
-            background-size:100%;
-            background-repeat: no-repeat;
-          }
-        `}
-        </style>
-      </div>
+      {isSettingOpen && (
+        <Modal onClose={() => setSettingOpen(false)}>
+          <SystemModal
+            title="設定"
+            button="確認"
+            subButton="還原預設"
+            onConfirm={() => setSettingOpen(false)}
+            customFunc={resetVolume}
+            onClose={() => setSettingOpen(false)}
+          >
+            <div className="h-full flex flex-col font-kai text-yellow-300 p-10">
+              <InputRange
+                label="音效音量"
+                value={soundValue}
+                onChange={setSoundValue}
+              />
+
+              <InputRange
+                label="音樂音量"
+                value={musicValue}
+                onChange={setMusicValue}
+              />
+            </div>
+          </SystemModal>
+        </Modal>
+      )}
     </>
   );
 }
