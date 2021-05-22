2588565af0d70ee0b788f76d88c6c3919c151d28
kayacchang <egg734631@gmail.com>
2021-05-22 12:14:30 +0800

    add npc

--- packages/api/routes/maps/index.js
+++ packages/api/routes/maps/index.js
@@ -4,6 +4,7 @@ import conditions from "./conditions.js";
 import rounds from "./rounds.js";
 import info from "./info.js";
 import unlock from "./unlock.js";
+import npc from "./npc.js";
 
 export default async function (fastify) {
   maps(fastify);
@@ -12,4 +13,5 @@ export default async function (fastify) {
   rounds(fastify);
   info(fastify);
   unlock(fastify);
+  npc(fastify);
 }
--- packages/client/src/components/common/Information.tsx
+++ packages/client/src/components/common/Information.tsx
@@ -38,13 +38,17 @@ export function Information() {
               button="確認"
               onConfirm={() => setOpen(false)}
             >
-              <div className="flex flex-col p-4 text-sm font-kai">
-                {information.map(({ label, value }) => (
-                  <div className="flex space-x-4 py-1">
-                    <h3>{label}:</h3>
-                    <p className="text-yellow-300">{value}</p>
-                  </div>
-                ))}
+              <div className="relative flex justify-center items-center w-full h-full">
+                <img src={Assets.Common.Modal_Frame_Information} alt="frame" />
+
+                <div className="absolute flex flex-col p-4 text-sm font-kai w-full">
+                  {information.map(({ label, value }) => (
+                    <div key={label} className="flex space-x-4 py-1">
+                      <h3>{label}:</h3>
+                      <p className="text-yellow-300">{value}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </SystemModal>
           </Modal>
