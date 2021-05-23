e77f0bea40b2375bfc6cea0ade1cbd0e5f21edae
kayacchang <egg734631@gmail.com>
2021-05-23 16:33:28 +0800

    add sound effect

--- packages/client/src/scenes/Room/Effect.tsx
+++ packages/client/src/scenes/Room/Effect.tsx
@@ -1,7 +1,9 @@
 import {
+  Effect,
   selectAssetsByName,
   selectRoomResult,
   selectRoomStatusCurrent,
+  useAppDispatch,
   useAppSelector,
 } from "system";
 import { memo, useMemo } from "react";
@@ -10,12 +12,14 @@ import { Game } from "layers";
 import { Spine } from "components";
 import clsx from "clsx";
 import { getViewPort } from "utils";
+import Sound from "assets/sound";
 
 export default memo(() => {
   const { width, height } = getViewPort();
   const status = useAppSelector(selectRoomStatusCurrent);
   const roundResult = useAppSelector(selectRoomResult);
   const assets = useAppSelector(selectAssetsByName);
+  const dispatch = useAppDispatch();
 
   const data = useMemo(() => {
     if (status !== RoomStatus.Result || !roundResult) {
@@ -28,8 +32,19 @@ export default memo(() => {
 
     const { game_round, ...rest } = roundResult.info;
     const target = Object.entries(rest).find(([, value]) => value);
-
     if (!target) return;
+
+    const result = target[0] as SkillOption;
+
+    const sound = {
+      bank_pair: Sound.Room.Skill_Fire,
+      player_pair: Sound.Room.Skill_IceHit,
+      player: Sound.Room.Skill_DoubleFire,
+      banker: Sound.Room.Skill_DoubleIceHit,
+      tie: Sound.Room.Skill_Wind,
+    }[result];
+
+    dispatch(Effect.play(sound));
 
     return {
       bank_pair: assets("Skill_FlameThrower_Spine"),
@@ -37,8 +52,8 @@ export default memo(() => {
       player: assets("Skill_FlareBlitz_Spine"),
       banker: assets("Skill_Blizzard_Spine"),
       tie: assets("Skill_Hurricane_Spine"),
-    }[target[0] as SkillOption];
-  }, [status, roundResult, assets]);
+    }[result];
+  }, [status, roundResult, assets, dispatch]);
 
   return (
     <Game
