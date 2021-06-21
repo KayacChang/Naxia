ec517e6803212e747bf2a8614b1389e8e3eb6865
skanda <anikiwayne@gmail.com>
2021-06-21 21:00:50 +0800

    added icons for the table

--- packages/client/src/components/Road/CountCube.tsx
+++ packages/client/src/components/Road/CountCube.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Assets from "assets";
 
 type CountCubePorps = {
   cubeClassName: string;
@@ -13,8 +14,42 @@ export function CountCube({
   count,
 }: CountCubePorps) {
   return (
-    <div className="flex items-center">
-      <div className={clsx("font-kai", cubeClassName)}>{role}</div>
+    <div className="flex items-center"> 
+      { role === '莊' &&
+        <img
+          className="p-2 pr-1"
+          src={Assets.Room.Icon_Fire}
+          alt="room frame big"
+        />
+      }
+      { role === '閒' &&
+        <img
+          className="p-2 pr-1"
+          src={Assets.Room.Icon_Ice}
+          alt="room frame big"
+        />
+      }
+      { role === '和' &&
+        <img
+          className="p-2 pr-1"
+          src={Assets.Room.Icon_Wind}
+          alt="room frame big"
+        />
+      }
+      { role === '莊對' &&
+        <img
+          className="p-2 pr-1"
+          src={Assets.Room.Icon_Light}
+          alt="room frame big"
+        />
+      }
+      { role === '閒對' &&
+        <img
+          className="p-2 pr-1"
+          src={Assets.Room.Icon_Dark}
+          alt="room frame big"
+        />
+      }
       <p className={clsx("px-4", color)}>{count}</p>
     </div>
   );
